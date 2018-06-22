d953d4dd8f140d3591a71c516fc33e76a7c29ce1
Giso Stallenberg <gisostallenberg@gmail.com>
2018-06-22 13:19:14 +0200

    Initial version

---
+++ src/ClauseHelper.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace GisoStallenberg\PDOClauseHelper;
+
+/**
+ * The clause helper
+ */
+class ClauseHelper
+{
+    /**
+     * Creates a new instance
+     *
+     * @return ClauseHelper
+     */
+    public static function create()
+    {
+        return new static();
+    }
+
+    /**
+     * Get the placeholder string for an IN clause and alters the values array so it maps the clause
+     *
+     * @param array $values
+     * @param string $prefix
+     * @return string
+     */
+    public function toInClausePlaceHolderString(array &$values, $prefix = null)
+    {
+        if (is_null($prefix)) {
+            $prefix = 'parameter';
+        }
+
+        $resultArray = [];
+        foreach (array_values($values) as $index => $value) {
+            $name = sprintf(
+                '%s%s',
+                $prefix,
+                $index
+            );
+            $resultArray[$name] = $value;
+        }
+        $values = $resultArray; // overwrite referenced array
+
+        return sprintf(
+            '(%s)',
+            implode(', ',
+                array_map(function($name) {
+                        return ':' . $name;
+                    },
+                    array_keys($resultArray)
+                )
+            )
+        );
+    }
+}
---
+++ tests/ClauseHelperTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace GisoStallenberg\PDOClauseHelper\Tests;
+
+use GisoStallenberg\PDOClauseHelper\ClauseHelper;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test the clause helper
+ */
+class ClauseHelperTest extends TestCase
+{
+
+    /**
+     * Test the to placeholder functionality
+     *
+     * @dataProvider toInClausePlaceHolderStringData
+     */
+    public function testToInClausePlaceHolderString($array, $prefix = null, $expectedString, $expectedArray)
+    {
+        $binder = ClauseHelper::create();
+
+        $this->assertNotEquals($array, $expectedArray);
+
+        $string = $binder->toInClausePlaceHolderString($array, $prefix);
+
+        $this->assertEquals($string, $expectedString);
+        $this->assertEquals($array, $expectedArray);
+    }
+
+    /**
+     * Data for the test
+     *
+     * @return array
+     */
+    public function toInClausePlaceHolderStringData()
+    {
+        return [
+            [
+                [
+                    'a',
+                    'b',
+                    'c',
+                ],
+                null,
+                '(:parameter0, :parameter1, :parameter2)',
+                [
+                    'parameter0' => 'a',
+                    'parameter1' => 'b',
+                    'parameter2' => 'c',
+                ],
+
+            ],
+            [
+                [
+                    153,
+                    158,
+                    186,
+                ],
+                'id',
+                '(:id0, :id1, :id2)',
+                [
+                    'id0' => 153,
+                    'id1' => 158,
+                    'id2' => 186,
+                ],
+
+            ],
+            [
+                [
+                    'a' => 153,
+                    'b' => 158,
+                    'c' => 186,
+                ],
+                'id',
+                '(:id0, :id1, :id2)',
+                [
+                    'id0' => 153,
+                    'id1' => 158,
+                    'id2' => 186,
+                ],
+
+            ]
+
+        ];
+    }
+}
